7b28d0ecfd82261fb733dc9cf261466c853a8a4f
rick <duyonggang@gmail.com>
2013-03-03 00:21:11 +0800

    init

---
+++ src/com/rick/typespeed/FullscreenActivity.java
@@ -0,0 +1,32 @@
+package com.rick.typespeed;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fullscreen);
+
+        final View controlsView = findViewById(R.id.fullscreen_content_controls);
+        final View contentView = findViewById(R.id.fullscreen_content);
+        
+    }
+    
+    private void startMonitor()
+    {
+    	Intent intent = new Intent(this, InputCheckService.class);
+    	startService(intent);
+    	
+    }
+
+}

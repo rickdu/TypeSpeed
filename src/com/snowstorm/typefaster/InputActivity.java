1201c872b6f2cd7e54b5ef896ed516aa3c7222da
rick <duyonggang@gmail.com>
2013-03-12 22:40:13 +0800

    src missed?

---
+++ src/com/snowstorm/typefaster/InputActivity.java
@@ -0,0 +1,24 @@
+package com.snowstorm.typefaster;
+
+import com.rick.typespeed.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class InputActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_input);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.input, menu);
+		return true;
+	}
+
+}

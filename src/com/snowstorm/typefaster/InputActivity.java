a04baa26008535e52a3dd8cc744c53aa19607cc1
rick <duyonggang@gmail.com>
2013-03-12 23:48:29 +0800

    did some tests on text components

--- src/com/snowstorm/typefaster/InputActivity.java
+++ src/com/snowstorm/typefaster/InputActivity.java
@@ -1,10 +1,23 @@
 package com.snowstorm.typefaster;
 
-import com.rick.typespeed.R;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextWatcher;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.text.style.URLSpan;
+import android.text.style.UnderlineSpan;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.rick.typespeed.R;
 
 public class InputActivity extends Activity {
 
@@ -12,6 +25,43 @@ public class InputActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_input);
+		TextView oriText = (TextView)findViewById(R.id.originalText);
+//		oriText.setText("abcdefghijklmnopqrstuvwxyz012345678\nabcdefghijklmnopqrstuvwxyz012345678");
+		
+		SpannableString ss = new SpannableString("红色打电话斜体删除线绿色下划线图片:.");
+
+	    //设置0-2的字符颜色
+	    ss.setSpan(new ForegroundColorSpan(Color.RED), 0, 2,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+	    //设置2-5的字符链接到电话簿，点击时触发拨号
+	    ss.setSpan(new URLSpan("tel:4155551212"), 2, 5,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+	    
+	    //设置9-11的字符为网络链接，点击时打开页面
+	    ss.setSpan(new URLSpan("http://www.hao123.com"), 9, 11,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+	    //设置13-15的字符点击时，转到写短信的界面，发送对象为10086
+	    ss.setSpan(new URLSpan("sms:10086"), 13, 15,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+	    //粗体
+	    ss.setSpan(new StyleSpan(Typeface.BOLD_ITALIC), 5, 7,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+	    //斜体
+	    ss.setSpan(new StyleSpan(android.graphics.Typeface.ITALIC), 7, 10,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+	  //下划线
+	    ss.setSpan(new UnderlineSpan(), 10, 16,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+	    //设置文本内容到textView
+	    oriText.setText(ss);
+
+	    //不添加这一句，拨号，http，发短信的超链接不能执行.
+	    oriText.setMovementMethod(LinkMovementMethod.getInstance());
+	    
+		oriText.setClickable(true);
+		
+		EditText userInputText = (EditText)findViewById(R.id.userText);
+		InputWatcher iw = new InputWatcher(oriText, userInputText);
+		userInputText.addTextChangedListener(iw); 
 	}
 
 	@Override
@@ -21,4 +71,34 @@ public class InputActivity extends Activity {
 		return true;
 	}
 
+}
+
+class InputWatcher implements TextWatcher {
+	private TextView tv;
+	private EditText et;
+	
+	public InputWatcher(TextView tv, EditText et) {
+		this.tv = tv;
+		this.et = et;
+	}
+	
+	@Override
+	public void afterTextChanged(Editable s) {
+		// TODO Auto-generated method stub
+		System.out.println("after text Changed");
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+		// TODO Auto-generated method stub
+		System.out.println("before text Changed");
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		// TODO Auto-generated method stub
+		System.out.println("text Changing");
+		this.tv.setText(this.tv.getText().toString() +  s.toString());
+	}
 }
